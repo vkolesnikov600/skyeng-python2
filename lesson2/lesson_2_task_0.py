d54b38e9324e10dba373f364519548b713600bbc
vkolesnikov600 <64896820+vkolesnikov600@users.noreply.github.com>
2024-04-16 13:11:03 +0600

    first commit

---
+++ lesson2/lesson_2_task_0.py
@@ -0,0 +1,4 @@
+my_age = input("Выедите ваш возраст: ")
+print(f"Ваш возраст: {my_age}")
+my_age = int(my_age) + 1
+print(f"Ваш возраст: {my_age}")
---
+++ lesson2/lesson_2_task_2.py
@@ -0,0 +1,7 @@
+def is_year_leap(year):
+    if year / 4 == 0:
+        print(True)
+    else:
+        print(False)
+            
+is_year_leap(2022)
